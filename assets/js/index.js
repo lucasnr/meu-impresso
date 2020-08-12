f01eae56605c43626908cc177c5a4a5649a0189a
Lucas do Nascimento <lucasnascimentoribeiro13@gmail.com>
2020-08-12 11:13:39 -0300

    Add nav tabs

---
+++ assets/js/index.js
@@ -0,0 +1,27 @@
+(function () {
+  const navbar = $('#navbar');
+  const nav = navbar.find('nav');
+  navbar.mouseenter(() => {
+    nav.css('display', 'block');
+  });
+  navbar.mouseleave(() => {
+    nav.css('display', 'none');
+  });
+
+  const tabTitle = nav.find('#tab-title');
+  const controls = nav.find('#tab-controls li');
+  let currentActive = 0;
+  controls.each((index, item) => {
+    const control = $(item);
+    control.mouseenter(() => {
+      $(controls[currentActive]).removeClass('active');
+      control.addClass('active');
+      tabTitle.text(control.text());
+
+      nav.find('.tab.active').removeClass('active');
+      nav.find(`#${control.data('tab-id')}`).addClass('active');
+
+      currentActive = index;
+    });
+  });
+})();
